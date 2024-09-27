036b943ea91ca427de6733b27065482163ba9de2
rogstark143 <akashnairvs@gmail.com>
2024-09-27 19:58:27 +1200

    FIX creds set up 7

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -13,70 +13,39 @@ const client = new SecretsManagerClient({
 
 let secretKey;
 
+let db;
+
 const getDb = function() {
     return db;
 }
 
-let db = null;
 const initialSetup = async function () {
     console.log("setting up db creds");
-
-    // try {
-    //     response = await client.send(
-    //         new GetSecretValueCommand({
-    //             SecretId: secret_name,
-    //             VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
-    //         })
-    //     ).then(response => {
-    //         console.log("the response string: " + response.SecretString);
-    //         secretKey = response.SecretString;
-    //     });
-    // } catch (error) {
-    //     console.log("The db credentials couldn't be accessed! ERROR: " + error);
-    //     throw error;
-    // }
 
     const config = { region: "us-east-1" }
 
     let secretsManager = new AWS.SecretsManager(config);
     // console.log("the manager: " + JSON.stringify(secretsManager));
     try {
-        let secretValue = secretsManager
-            .getSecretValue({ SecretId: secret_name })
-            .promise()
-            .then(response => {
-                console.log("finished the request");
-                console.log("The response: " + response);
-                console.log("the response json: " + JSON.stringify(response));
+        const secretValue = await client.send(new GetSecretValueCommand({
+            SecretId: secret_name,
+            VersionStage: "AWSCURRENT",
+        }));
 
-                var creds = JSON.parse(response.SecretString);
+        // Process the secretValue
+        const creds = JSON.parse(secretValue.SecretString);
 
-                console.log("The db creds: " + creds);
-                console.log("The db creds string: " + JSON.stringify(creds));
+        AWS.config.update({
+            accessKeyId: creds.db_access_key_id,
+            secretAccessKey: creds.db_secret_access_key,
+            region: "us-east-1",
+            endpoint: "https://dynamodb.us-east-1.amazonaws.com"
+        });
 
-                AWS.config.update({
+        db = new AWS.DynamoDB.DocumentClient({ convertEmptyValues: true });
 
-                    accessKeyId: creds.db_access_key_id,
-                    secretAccessKey: creds.db_secret_access_key,
-                    region: "us-east-1",
-                    endpoint: "https://dynamodb.us-east-1.amazonaws.com"
-                });
+        console.log("DB object initialized:", JSON.stringify(db));
 
-                db = new AWS.DynamoDB.DocumentClient({ convertEmptyValues: true });
-
-                console.log("The db object in the setup phase: " + db);
-                console.log("The db object in the setup phase string: " + JSON.stringify(db));
-            })
-            .then(() => {
-                console.log("the db after then: " + JSON.stringify(db));
-            });
-
-        // if ('SecretString' in secretValue) {
-        //     return secret = secretValue.SecretString;
-        // } else {
-        //     let buff = new Buffer(secretValue.SecretBinary, 'base64');
-        //     return decodedBinarySecret = buff.toString('ascii');
-        // }
     } catch (error) {
         console.log("The db credentials couldn't be accessed! ERROR: " + error);
     }
