a8ebf428e3964029ac73bc6252260e9f07fa1c40
rogstark143 <akashnairvs@gmail.com>
2024-10-04 00:27:09 +1300

    ADD loadbalancer domain to cors 2

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -2,7 +2,7 @@ const utilConstants = {
     TOKEN_HEADER_KEY: "X_H_ACCESS_KEY_HEADER",
     SNS_TOPIC_ARN: "arn:aws:sns:us-east-1:017820658756:event_reminder",
     SNS_SUBSCRIPTION_PROTOCOL: "email",
-    EC2_IP: "load-balancer-hackathon-385661405.us-east-1.elb.amazonaws.com"
+    EC2_IP: "frontend-loadbalancer-116657429.us-east-1.elb.amazonaws.com"
 };
 
 module.exports = {
